27d1ef60af4725f7f991889d248419c7d49ef9d7
Martin Kovac <martin.kovac@myunidays.com>
2019-09-24 16:58:54 +0100

    Add Dataloaders

--- BackEnd/Infrastructure/EntityExtensions.cs
+++ BackEnd/Infrastructure/EntityExtensions.cs
@@ -21,5 +21,63 @@ namespace BackEnd.Infrastructure
                         })
                     .ToList()
             };
+
+        public static ConferenceDTO.SessionResponse MapSessionResponse(this Session session) =>
+            new ConferenceDTO.SessionResponse
+            {
+                ID = session.ID,
+                Title = session.Title,
+                StartTime = session.StartTime,
+                EndTime = session.EndTime,
+                Tags = session.SessionTags?
+                    .Select(st => new ConferenceDTO.Tag
+                    {
+                        ID = st.TagId,
+                        Name = st.Tag.Name
+                    })
+                    .ToList(),
+                Speakers = session.SessionSpeakers?
+                    .Select(ss => new ConferenceDTO.Speaker
+                    {
+                        ID = ss.SpeakerId,
+                        Name = ss.Speaker.Name
+                    })
+                    .ToList(),
+                TrackId = session.TrackId,
+                Track = new ConferenceDTO.Track
+                {
+                    TrackId = session?.TrackId ?? 0,
+                    Name = session.Track?.Name
+                },
+                ConferenceID = session.ConferenceID,
+                Abstract = session.Abstract
+            };
+
+        public static ConferenceDTO.AttendeeResponse MapAttendeeResponse(this Attendee attendee) =>
+            new ConferenceDTO.AttendeeResponse
+            {
+                ID = attendee.ID,
+                FirstName = attendee.FirstName,
+                LastName = attendee.LastName,
+                UserName = attendee.UserName,
+                Sessions = attendee.SessionAttendees?
+                    .Select(s =>
+                        new ConferenceDTO.Session
+                        {
+                            ID = s.SessionId,
+                            Title = s.Session.Title,
+                            StartTime = s.Session.StartTime,
+                            EndTime = s.Session.EndTime
+                        })
+                    .ToList(),
+                Conferences = attendee.ConferenceAttendees?
+                    .Select(ca =>
+                        new ConferenceDTO.Conference
+                        {
+                            ID = ca.ConferenceId,
+                            Name = ca.Conference.Name
+                        })
+                    .ToList(),
+            };
     }
 }
