fabe45f8db4e1315576195ba53d690d3801dffad
Martin Kovac <martin.kovac@myunidays.com>
2019-09-24 08:44:36 +0100

    Add refactor migration

--- BackEnd/Data/ApplicationDbContext.cs
+++ BackEnd/Data/ApplicationDbContext.cs
@@ -10,6 +10,45 @@ namespace BackEnd.Data
             
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Attendee>()
+                .HasIndex(a => a.UserName)
+                .IsUnique();
+
+            // Ignore computed property
+            modelBuilder.Entity<Session>()
+                .Ignore(s => s.Duration);
+
+            // Many-to-many: Conference <-> Attendee
+            modelBuilder.Entity<ConferenceAttendee>()
+                .HasKey(ca => new { ca.ConferenceId, ca.AttendeeId });
+
+            // Many-to-many: Session <-> Attendee
+            modelBuilder.Entity<SessionAttendee>()
+                .HasKey(ca => new { ca.SessionId, ca.AttendeeId });
+
+            // Many-to-many: Speaker <-> Session
+            modelBuilder.Entity<SessionSpeaker>()
+                .HasKey(ss => new { ss.SessionId, ss.SpeakerId });
+
+            // Many-to-many: Session <-> Tag
+            modelBuilder.Entity<SessionTag>()
+                .HasKey(st => new { st.SessionId, st.TagId });
+        }
+
+        public DbSet<Conference> Conferences { get; set; }
+
+        public DbSet<Session> Sessions { get; set; }
+
+        public DbSet<Track> Tracks { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+
         public DbSet<Speaker> Speakers { get; set; }
+
+        public DbSet<Attendee> Attendees { get; set; }
     }
 }
