7b08cd465dec5e7f484688264340df108c6fcf39
Martin Kovac <martin.kovac@myunidays.com>
2019-09-24 08:01:57 +0100

    Add ConferenceDtos

---
+++ ConferenceDTO/Track.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConferenceDTO
+{
+    public class Track
+    {
+        public int TrackId { get; set; }
+
+        [Required]
+        public int ConferenceId { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
+    }
+}
