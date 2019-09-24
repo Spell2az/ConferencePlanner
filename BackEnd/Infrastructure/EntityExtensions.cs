752751452d3684b92bbfbf2fb71e7e5f33d05724
Martin Kovac <martin.kovac@myunidays.com>
2019-09-24 09:02:33 +0100

    Add dto mapper as extension method

---
+++ BackEnd/Infrastructure/EntityExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using BackEnd.Data;
+
+namespace BackEnd.Infrastructure
+{
+    public static class EntityExtensions
+    {
+        public static ConferenceDTO.SpeakerResponse MapSpeakerResponse(this Speaker speaker) =>
+            new ConferenceDTO.SpeakerResponse
+            {
+                ID = speaker.ID,
+                Name = speaker.Name,
+                Bio = speaker.Bio,
+                WebSite = speaker.WebSite,
+                Sessions = speaker.SessionSpeakers?
+                    .Select(ss =>
+                        new ConferenceDTO.Session
+                        {
+                            ID = ss.SessionId,
+                            Title = ss.Session.Title
+                        })
+                    .ToList()
+            };
+    }
+}
